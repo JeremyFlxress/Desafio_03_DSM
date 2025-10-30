9517fc0aff66c824d74f57507f4740e7a5af3e47
Josué García <josueduardo.dev@gmail.com>
2025-10-30 06:12:22 -0600

    rest api base

---
+++ app/src/main/java/com/example/desafiopractico3/network/RestCountriesApiService.kt
@@ -0,0 +1,20 @@
+package com.example.desafiopractico3.network
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+import com.example.desafiopractico3.model.CountryResponse
+import retrofit2.http.Query
+
+interface RestCountriesApiService {
+
+    @GET("v3.1/all")
+    suspend fun getAllCountries(@Query("fields") fields: String = "name,capital,region,subregion,population,flags,currencies,languages,latlng"): List<CountryResponse>
+
+    // @Path reemplaza "{regionName}" con el valor de la variable
+    @GET("v3.1/region/{regionName}")
+    suspend fun getCountriesByRegion(
+        @Path("regionName") region: String,
+        @Query("fields") fields: String = "name,capital,flags,latlng,subregion,population,currencies,languages"
+    ): List<CountryResponse>
+
+}
---
+++ app/src/main/java/com/example/desafiopractico3/network/RestCountriesClient.kt
@@ -0,0 +1,31 @@
+package com.example.desafiopractico3.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+// Usamos 'object' para crear un Singleton
+object RestCountriesClient {
+
+    // URL Base de la API de RestCountries
+    private const val BASE_URL = "https://restcountries.com/"
+
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val apiService: RestCountriesApiService by lazy {
+        retrofit.create(RestCountriesApiService::class.java)
+    }
+}
